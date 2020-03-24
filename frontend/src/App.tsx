c89291330d069105122e805a808cb1ade811f1a6
Roman Tcharni <roman.tcharni@gmail.com>
2020-03-24 16:00:48 +0200

    dynamic work fields added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import FrontpageLayout from "./components/frontpage/Frontpage-layout";
 import AddWorkEntryForm from "./components/worktimepage/AddWorkEntryForm";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Config } from "../../models";
+
+const mockConfig: Config = {
+  workFormFields: ["workTitle", "workDetails", "customerName"]
+};
 
 export default function App() {
+  // fetch config data for logged in customer
+  // add context global store
+
+  const [config, setConfig] = useState<Config | null>(null);
+  useEffect(() => {
+    console.log("App mounted, use effect");
+    setConfig(mockConfig);
+    // return () => {
+    // do cleanup
+    // }
+  }, []);
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/addworkentry" component={() => <AddWorkEntryForm />} />
+          <Route
+            path="/addworkentry"
+            component={() => <AddWorkEntryForm config={config as Config} />}
+          />
           <Route path="/" component={() => <FrontpageLayout />} />
         </Switch>
       </div>
--- frontend/src/components/worktimepage/AddWorkEntryForm.tsx
+++ frontend/src/components/worktimepage/AddWorkEntryForm.tsx
@@ -1,55 +1,127 @@
 import React, { useState, Props, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import { Container } from "@material-ui/core";
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
+import { Config } from "../../../../models";
 
-const AddWorkEntryForm = (props: Props<any>) => {
-  const [state, setState] = React.useState({
-    workTitle: "Work title",
-    workDetails: "Work details"
-  });
+type AppProps = { config: Config };
+type AllFields = {
+  workTitle?: string;
+  workDetails?: string;
+  customerName?: string;
+};
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log(event.target);
+const AddWorkEntryForm = ({ config }: AppProps) => {
+  // const [state, setState] = useState({
+  //   workTitle: "Work title",
+  //   workDetails: "Work details",
+  //   customerName: "Customer name"
+  // });
+  const [fields, setFields] = useState({} as AllFields);
+
+  useEffect((): void => {
+    // console.log(config);
+    if (config) {
+      let formFields: any = {};
+      for (const k of config.workFormFields) {
+        formFields[k] = "";
+      }
+      console.log("setting");
+      setFields(formFields);
+    }
+  }, []);
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     switch (event.target.name) {
       case "workTitle":
-        setState({ ...state, workTitle: event.target.value });
+        setFields({ ...fields, workTitle: event.target.value });
         break;
       case "workDetails":
-        setState({ ...state, workDetails: event.target.value });
+        setFields({ ...fields, workDetails: event.target.value });
+        break;
+      case "customerName":
+        setFields({ ...fields, customerName: event.target.value });
         break;
     }
-    // console.log(state);
   };
 
-  useEffect(() => {
-    console.log(state);
-  });
+  const handleSaveButton = (): void => {
+    if (validateForm()) {
+      // form good make request to backend
+      console.log("Form Good");
+    } else {
+      console.log("Form Bad");
+    }
+  };
+
+  const validateForm = (): boolean => {
+    for (const inputValue of Object.values(fields)) {
+      if (!inputValue) {
+        return false;
+      }
+    }
+    return true;
+  };
 
   return (
     <React.Fragment>
       <Container>
         <h4>Add new work entry</h4>
+        {fields.workTitle !== undefined && (
+          <div className="row">
+            <TextField
+              required
+              name="workTitle"
+              label="Title"
+              variant="outlined"
+              value={fields.workTitle}
+              onChange={handleInputChange}
+              error={fields.workTitle === "" ? true : false}
+              helperText={fields.workTitle === "" ? "Required field" : ""}
+            />
+          </div>
+        )}
+        {fields.workDetails !== undefined && (
+          <div className="row">
+            <TextField
+              required
+              name="workDetails"
+              label="Details"
+              variant="outlined"
+              value={fields.workDetails}
+              onChange={handleInputChange}
+              error={fields.workDetails === "" ? true : false}
+              helperText={fields.workDetails === "" ? "Required field" : ""}
+            />
+          </div>
+        )}
 
-        <TextField
-          name="workTitle"
-          label="Title"
-          value={state.workTitle}
-          onChange={handleInputChange}
+        {fields.customerName !== undefined && (
+          <div className="row">
+            <TextField
+              required
+              name="customerName"
+              label="Customer name"
+              variant="outlined"
+              value={fields.customerName}
+              onChange={handleInputChange}
+              error={fields.customerName === "" ? true : false}
+              helperText={fields.customerName === "" ? "Required field" : ""}
+            />
+          </div>
+        )}
+
+        <Button
           variant="outlined"
-        />
-        <TextField
-          name="workDetails"
-          label="Details"
-          value={state.workDetails}
-          onChange={handleInputChange}
-          variant="outlined"
-        />
+          color="default"
+          startIcon={<AddRoundedIcon />}
+          onClick={handleSaveButton}
+        >
+          Save
+        </Button>
       </Container>
     </React.Fragment>
   );
