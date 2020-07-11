5ab38af3270fe6a3dd4e5ed68b1a1760b1382327
Roman Tcharni <roman.tcharni@gmail.com>
2020-07-11 10:34:34 +0300

    feat(workentrypage): added top tabs

--- backend/client/src/modules/material.module.ts
+++ backend/client/src/modules/material.module.ts
@@ -3,9 +3,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule],
-  exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
