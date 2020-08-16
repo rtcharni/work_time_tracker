9bfa3cae5701059198c0bdeb75a134d702217e52
Roman Tcharni <roman.tcharni@gmail.com>
2020-08-16 10:36:27 +0300

    added dubug true in nodemailer

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,17 +1,17 @@
-import nodemailer, { SendMailOptions } from "nodemailer";
-import Mail from "nodemailer/lib/mailer";
-import { User } from "../../models";
+import nodemailer, { SendMailOptions } from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import { User } from '../../models';
 
 function createTransporter(): Mail {
   try {
     return nodemailer.createTransport({
-      service: "gmail",
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
       },
       secure: true,
-      //   debug: true,
+      debug: true,
       logger: true,
     });
   } catch (error) {
@@ -24,20 +24,17 @@ export class EmailService {
   private static transporter: Mail = createTransporter();
   // TODO: pretty HTML format
   private static defaultOptions: SendMailOptions = {
-    from: "Roman Tcharni roman.tcharni@gmail.com", // real app name and new email address
-    // to: "r_t88@msn.com", // null
-    subject: "Work App - ",
+    from: 'Work App work.app.general@gmail.com', // real app name and new email address
+    subject: 'Work App - ',
   };
 
   public static async sendRecoveryLink(email: string, passwordToken: string) {
     const options = Object.assign({}, EmailService.defaultOptions, {
       to: email,
       text: `You issued a forgot password recovery process. Follow this link to reset your password to new one. Recovery process is active for 30 minutes. If you did not issue this recovery request then please do NOT do anything, because somebody is trying to issue this recovery on your behalf. Here is password recovery link for you to follow: ${
-        process.env.NODE_ENV === "production"
-          ? "https://workapp-dev.herokuapp.com/resetpassword/"
-          : "http://localhost:4200/resetpassword/"
+        process.env.NODE_ENV === 'production' ? 'https://workapp-dev.herokuapp.com/resetpassword/' : 'http://localhost:4200/resetpassword/'
       }${passwordToken}`,
-      subject: EmailService.defaultOptions.subject + "Password recovery link",
+      subject: EmailService.defaultOptions.subject + 'Password recovery link',
     });
     const res = await EmailService.transporter.sendMail(options);
     console.log(`Res from sendMail function`);
@@ -49,9 +46,7 @@ export class EmailService {
       to: user.email,
       text: `We are pleased to have you onboard, Your account is set up and ready to use. You can log in from here: https://workapp-dev.herokuapp.com/ .
       Your User ID is ${user.userId}`,
-      subject:
-        EmailService.defaultOptions.subject +
-        `Welcome to Work App ${user.firstName}!`,
+      subject: EmailService.defaultOptions.subject + `Welcome to Work App ${user.firstName}!`,
     });
     const res = await EmailService.transporter.sendMail(options);
     console.log(`Res from sendMail function`);
