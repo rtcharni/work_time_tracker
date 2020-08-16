41cc3690de7bc905e5b83fd06b05919059ad28fe
Roman Tcharni <roman.tcharni@gmail.com>
2020-08-16 19:45:19 +0300

    added routes

--- src/routes/database.routes.ts
+++ src/routes/database.routes.ts
@@ -1,9 +1,5 @@
-import { Router } from "express";
-import {
-  WorkEntriesController,
-  CompaniesController,
-  UsersController,
-} from "../controllers";
+import { Router } from 'express';
+import { WorkEntriesController, CompaniesController, UsersController } from '../controllers';
 
 export class DatabaseRoutes {
   public router: Router;
@@ -13,6 +9,7 @@ export class DatabaseRoutes {
     this.initWorkEntryRoutes();
     this.initCompaniesRoutes();
     this.initUsersRoutes();
+    this.initWorkMessagesRoutes();
   }
 
   /**
@@ -21,33 +18,38 @@ export class DatabaseRoutes {
   private initWorkEntryRoutes(): void {
     const workEntryController = new WorkEntriesController();
     this.router.use(
-      "/workentries",
+      '/workentries',
       Router()
-        .get("", workEntryController.getWorkEntries())
-        .post("", workEntryController.addWorkEntry())
-        .put("/:workEntryId", workEntryController.editWorkEntry())
-        .delete("/:workEntryId", workEntryController.deleteWorkEntry())
+        .get('', workEntryController.getWorkEntries())
+        .post('', workEntryController.addWorkEntry())
+        .put('/:workEntryId', workEntryController.editWorkEntry())
+        .delete('/:workEntryId', workEntryController.deleteWorkEntry())
     );
   }
 
   private initCompaniesRoutes(): void {
     const companiesController = new CompaniesController();
-    this.router.use(
-      "/companies",
-      Router().get("", companiesController.getCompanies())
-    );
+    this.router.use('/companies', Router().get('', companiesController.getCompanies()));
   }
 
   private initUsersRoutes(): void {
     const usersController = new UsersController();
     this.router.use(
-      "/users",
+      '/users',
       Router()
         // .post("/login", usersController.logInUser())
-        .get("", usersController.getUsers())
-        .post("", usersController.addUser())
-        .patch("/:userId", usersController.editUser())
-        .delete("/:userId", usersController.deleteUser())
+        .get('', usersController.getUsers())
+        .post('', usersController.addUser())
+        .patch('/:userId', usersController.editUser())
+        .delete('/:userId', usersController.deleteUser())
+    );
+  }
+
+  private initWorkMessagesRoutes(): void {
+    const usersController = new WorkMessagesController();
+    this.router.use(
+      '/workmessages',
+      Router().get('', null).post('', null).patch('/:workMessageId', null).delete('/:workMessageId', null)
     );
   }
 }
