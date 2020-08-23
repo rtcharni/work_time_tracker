cf04c45bf787764f2ef611e2b89fb448cb215d58
Roman Tcharni <roman.tcharni@gmail.com>
2020-08-23 18:02:52 +0300

    good

--- client/src/app/pages/messaging-page/workmessages-tab/workmessages-tab.component.ts
+++ client/src/app/pages/messaging-page/workmessages-tab/workmessages-tab.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkEntryService, UserService } from '@services';
 import { UserAndCompany, WorkEntry } from '@models';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-workmessages-tab',
@@ -11,21 +13,35 @@ export class WorkmessagesTabComponent implements OnInit {
   user: UserAndCompany;
   workEntries: WorkEntry[] = [];
 
+  // 2 months by default
+  startDate: Date = moment()
+    .month(moment().month() - 2)
+    .toDate();
+  endDate: Date = new Date();
+
   constructor(private userService: UserService, private workEntryService: WorkEntryService) {}
 
-  async ngOnInit() {
+  async ngOnInit(): Promise<void> {
     this.user = this.userService.getUser();
-    this.workEntries = await this.getWorkEntries();
+    this.workEntries = await this.getWorkEntries(this.startDate, this.endDate);
   }
 
-  async getWorkEntries() {
+  async getWorkEntries(startDate: Date, endDate: Date): Promise<WorkEntry[]> {
     const workEntries = await this.workEntryService.getWorkEntries(
       null,
       null,
       this.user.companyId,
-      '2020-01-01T10:00:00.000Z',
-      '2020-09-01T10:00:00.000Z'
+      startDate.toISOString(),
+      endDate.toISOString()
     );
     return workEntries;
+  }
+
+  async handleDateChange(startOrEnd: 'start' | 'end', event: MatDatepickerInputEvent<Date>): Promise<void> {
+    this.startDate = startOrEnd === 'start' ? event.value : this.startDate;
+    this.endDate = startOrEnd === 'end' ? event.value : this.endDate;
+    if (startOrEnd === 'end' && this.startDate && this.endDate) {
+      this.workEntries = await this.getWorkEntries(this.startDate, this.endDate);
+    }
   }
 }
