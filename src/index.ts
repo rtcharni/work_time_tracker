ca6cbc03c9a640eed84cb3ccd9a6e1e0bcb61647
Roman Tcharni <roman.tcharni@gmail.com>
2020-08-23 10:13:35 +0300

    added types for module-alias and use it only in production

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 // tslint:disable-next-line: no-var-requires
-require('module-alias/register');
+// require('module-alias/register');
+import moduleAlias from 'module-alias';
+
+if (process.env.NODE_ENV === 'production') {
+  moduleAlias.addAliases({
+    '@models': 'dist/models',
+    '@controllers': 'dist/src/controllers',
+    '@services': 'dist/src/services',
+    '@mockdata': 'dist/src/mockdata',
+  });
+}
+
 import { App } from './app';
 // tslint:disable-next-line: no-var-requires
 require('dotenv').config();
