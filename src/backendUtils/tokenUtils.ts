9a7871c523f5654f28613edd38cd1780bae8ba20
Roman Tcharni <roman.tcharni@gmail.com>
2020-11-04 16:09:32 +0200

    wip request action token validation middleware

--- src/backendUtils/tokenUtils.ts
+++ src/backendUtils/tokenUtils.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { User } from '@models';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler } from 'express';
+import { ValidationChain } from 'express-validator';
 
 export class TokenManagement {
   public static generateLoginToken(user: User): string {
@@ -49,4 +50,59 @@ export class TokenManagement {
       return;
     }
   }
+
+  public static validateRequestActionMiddleware(reqPropCheck: RequestPropertyCheck[]): RequestHandler {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        console.log('IN validateRequestActionMiddleware');
+        const token = jwt.decode(req.cookies.auth) as {
+          [key: string]: any;
+        };
+
+        if (!this.requestToOwnCompany(req, token.companyId)) {
+          return res.status(403).send(`Request is not allowed`);
+        }
+
+        if (token.admin) {
+          return next();
+        }
+
+        for (const checkProp of reqPropCheck) {
+          if (token[checkProp.propName] !== req[checkProp.locatedInRequest][checkProp.propName]) {
+            console.error(
+              `Request is not allowed. Token property ${checkProp.propName} with value ${
+                token[checkProp.propName]
+              } is not matching request value of ${req[checkProp.locatedInRequest][checkProp.propName]}`
+            );
+            return res.status(403).send(`Request is not allowed`);
+          }
+        }
+
+        return next();
+      } catch (error) {
+        console.error(`Error while validating request action`);
+      }
+    };
+  }
+
+  private static requestToOwnCompany(req: Request, tokenCompany: number): boolean {
+    if (req.query.companyId && +req.query.companyId !== tokenCompany) {
+      console.error(`Request is not allowed. Company ${tokenCompany} is trying access other company ${req.query.companyId}`);
+      return false;
+    }
+    if (req.body.companyId && +req.body.companyId !== tokenCompany) {
+      console.error(`Request is not allowed. Company ${tokenCompany} is trying access other company ${req.body.companyId}`);
+      return false;
+    }
+    if (req.params.companyId && +req.params.companyId !== tokenCompany) {
+      console.error(`Request is not allowed. Company ${tokenCompany} is trying access other company ${req.params.companyId}`);
+      return false;
+    }
+    return true;
+  }
+}
+
+interface RequestPropertyCheck {
+  propName: string;
+  locatedInRequest: 'query' | 'body' | 'params';
 }
