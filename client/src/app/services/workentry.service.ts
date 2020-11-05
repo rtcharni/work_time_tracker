a794a596239e3e848babd4395e9526186303a65d
Roman Tcharni <roman.tcharni@gmail.com>
2020-11-05 10:28:09 +0200

    removed worker that breaks build

--- client/src/app/services/workentry.service.ts
+++ client/src/app/services/workentry.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { WorkEntry } from '@models';
-import { worker } from 'cluster';
 
 @Injectable({
   providedIn: 'root',
